90f7ea9a060d3ec295047a316ccbdfb172320142
Mai Yia Xiong <maiyiax@gmail.com>
2020-09-16 18:22:35 -0500

    updated the for loop section

--- assets/js/game.js
+++ assets/js/game.js
@@ -76,7 +76,19 @@ var fight = function(enemyName) {
 
 
 for(var i =0; i < enemyNames.length; i++) {
+  if (playerHealth > 0) {
+    window.alert("Welcome to Robot Gladiators! Round " + (i +1));
   var pickedEnemyName = enemyNames[i];
   enemyHealth = 50;
-  fight(pickedEnemyName);
+  // use debugger to pause script from running and check what's going on at that moment in the code
+    // debugger;
+    debugger;
+
+    // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+    fight(pickedEnemyName);
+}
+  else {
+    window.alert("You have lost your robot in battle! Game Over!");
+    break;
+  }
 }
